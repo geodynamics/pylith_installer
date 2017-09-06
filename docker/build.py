4e9ce0d114544cb8c49316b7751397b7c059d3a3
Brad Aagaard <baagaard@usgs.gov>
2017-09-06 09:38:04 -0700

    Updated docker install for v2.2.1.

--- docker/build.py
+++ docker/build.py
@@ -9,7 +9,7 @@
 # This code was developed as part of the Computational Infrastructure
 # for Geodynamics (http://geodynamics.org).
 #
-# Copyright (c) 2010-2016 University of California, Davis
+# Copyright (c) 2010-2017 University of California, Davis
 #
 # See COPYING for license information.
 #
@@ -21,7 +21,7 @@ class DockerApp(object):
 
     def __init__(self, container):
         arch = "debian"
-        pylithVersion = "2.2.1rc1"
+        pylithVersion = "2.2.1"
         buildVersion = "latest"
 
         baseenvTag = buildVersion
